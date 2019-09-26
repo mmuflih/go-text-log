201ac8254efd18efebfef7bd17c4695652b00c47
Muflih <muflic.24@gmail.com>
2019-09-26 16:42:24 +0700

    Add datalog to text

---
+++ log.go
@@ -0,0 +1,39 @@
+package datalog
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type DataLog interface {
+	Write(err error, data ...interface{})
+}
+
+type dataLog struct {
+	fileName string
+}
+
+func New(fileName string) DataLog {
+	return &dataLog{fileName}
+}
+
+func (dl dataLog) Write(e error, data ...interface{}) {
+	f, err := os.OpenFile(dl.fileName, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		f, err = os.Create(dl.fileName)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer f.Close()
+	d, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err, "marshal")
+	}
+	text := `error with exception "` + e.Error() + `" data => ` + string(d)
+	if _, err = f.WriteString(text + "\n"); err != nil {
+		panic(err)
+	}
+}
